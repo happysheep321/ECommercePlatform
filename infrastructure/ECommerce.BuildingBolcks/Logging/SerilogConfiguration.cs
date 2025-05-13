81b2444afc5cca28150ece7eb289528955422cd1
青蛮 <19810753794@139.com>
2025-05-13 23:32:38 +0800

    初步在工具类项目中创建Serilog日志配置类

---
+++ infrastructure/ECommerce.BuildingBolcks/Logging/SerilogConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Events;
+
+namespace ECommerce.BuildingBlocks.Logging;
+
+/// <summary>
+/// 提供 Serilog 日志配置的静态类
+/// </summary>
+public static class SerilogConfiguration
+{
+    /// <summary>
+    /// 配置并初始化 Serilog 日志记录器
+    /// </summary>
+    /// <param name="configuration">应用程序的配置对象</param>
+    public static void ConfigureSerilog(IConfiguration configuration)
+    {
+        // 创建并配置 Serilog 日志记录器
+        var logger = new LoggerConfiguration()
+            .MinimumLevel.Information() // 设置默认最小日志级别为 Information
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning) // Microsoft 命名空间日志级别为 Warning
+            .MinimumLevel.Override("System", LogEventLevel.Warning) // System 命名空间日志级别为 Warning
+            .Enrich.FromLogContext() // 启用日志上下文信息
+            .WriteTo.Console() // 输出到控制台
+            .ReadFrom.Configuration(configuration) // 从配置文件读取其他设置
+            .CreateLogger();
+
+        // 设置全局日志记录器
+        Log.Logger = logger;
+    }
+}
